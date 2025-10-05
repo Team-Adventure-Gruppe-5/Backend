8537e9ece582969d1407e7886f12641588d775c9
laerkeloenborg <lalo0001@stud.kea.dk>
2025-10-05 19:48:24 +0200

    - Endpoint to get a specific activity created

--- src/main/java/com/example/adventurexpbackend/controller/ActivityController.java
+++ src/main/java/com/example/adventurexpbackend/controller/ActivityController.java
@@ -3,13 +3,16 @@ package com.example.adventurexpbackend.controller;
 import com.example.adventurexpbackend.model.Activity;
 import com.example.adventurexpbackend.repository.ActivityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 public class ActivityController {
+
     @Autowired
     ActivityRepo activityRepo;
 
@@ -18,4 +21,14 @@ public class ActivityController {
         return activityRepo.findAll();
     }
 
+    @GetMapping("/activity/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable int id){
+        Optional<Activity> activity = activityRepo.findById(id);
+
+        if(activity.isPresent()){
+            return ResponseEntity.ok(activity.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
