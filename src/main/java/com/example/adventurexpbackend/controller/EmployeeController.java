e66cc2da6ba643351a0965fb657098fd4f72bcaf
CamillaEva <caha0007@stud.kea.dk>
2025-10-06 13:50:53 +0200

    endpoint for employees

--- src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
+++ src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.adventurexpbackend.controller;
 
 
+import com.example.adventurexpbackend.model.Employee;
 import com.example.adventurexpbackend.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -12,4 +16,9 @@ public class EmployeeController {
 
     @Autowired
     EmployeeRepo employeeRepo;
+
+    @GetMapping("/employees")
+    public List<Employee> employeepage(){
+        return employeeRepo.findAll();
+    }
 }
