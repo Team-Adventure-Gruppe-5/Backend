1388349a8e78247af68fb03f9c25a538dfca03ee
KasperPersson <kape0010@stud.kea.dk>
2025-10-10 10:05:57 +0200

    post mapping for create emp

--- src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
+++ src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
@@ -4,9 +4,8 @@ package com.example.adventurexpbackend.controller;
 import com.example.adventurexpbackend.model.Employee;
 import com.example.adventurexpbackend.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +21,9 @@ public class EmployeeController {
         return employeeRepo.findAll();
     }
 
+    @PostMapping("/employees")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepo.save(employee);
+    }
 }
