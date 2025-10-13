8511dad219ecde8752d1a868bc656790089f234d
KasperPersson <kape0010@stud.kea.dk>
2025-10-13 13:47:13 +0200

    #SCRUM-43 DeleteMapping for employees.

--- src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
+++ src/main/java/com/example/adventurexpbackend/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import com.example.adventurexpbackend.model.Employee;
 import com.example.adventurexpbackend.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -25,5 +27,16 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepo.save(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<String> deleteEmp(@PathVariable("id") Integer id) {
+        Optional<Employee> optionalEmployee = employeeRepo.findById(id);
+        if (optionalEmployee.isPresent()) {
+            employeeRepo.deleteById(id);
+            return ResponseEntity.ok("Employee deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not Found");
+        }
     }
 }
