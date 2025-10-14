e31e8613063910bf5f08e30609a316e8392de236
laerkeloenborg <lalo0001@stud.kea.dk>
2025-10-14 19:22:32 +0200

    - show customers info + bookings

--- src/main/java/com/example/adventurexpbackend/repository/CustomerRepo.java
+++ src/main/java/com/example/adventurexpbackend/repository/CustomerRepo.java
@@ -1,11 +1,15 @@
 package com.example.adventurexpbackend.repository;
 
+import com.example.adventurexpbackend.model.Booking;
 import com.example.adventurexpbackend.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Integer> {
 
     Customer findByMail(String mail);
+
 }
