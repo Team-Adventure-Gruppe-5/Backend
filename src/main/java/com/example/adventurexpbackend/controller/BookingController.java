748dd810c3f430f2f8cc0c0b8db6554f023d4d44
laerkeloenborg <lalo0001@stud.kea.dk>
2025-10-15 21:03:35 +0200

    removed unused imports

--- src/main/java/com/example/adventurexpbackend/controller/BookingController.java
+++ src/main/java/com/example/adventurexpbackend/controller/BookingController.java
@@ -11,9 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
-import java.awt.print.Book;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
